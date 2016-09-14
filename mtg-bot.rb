5e57e7e8b0bd44fec650f2851a6cd34b5e3dd872
Robert Cochran <rcochran@manifestcorp.com>
2016-09-14 19:39:40 -0400

    new json attempt to line up with what slack is looking for

--- mtg-bot.rb
+++ mtg-bot.rb
@@ -1,10 +1,12 @@
 require 'sinatra'
 require 'mtg_sdk'
+require 'json'
 
 get "/mtg-bot" do 
 	name = params["name"] || params["text"]
 	cards = MTG::Card.where(name: name).all
 	text = "No match found."
+	
 	if(cards.any?)
 		card = cards.find { |c| c.image_url != nil and c.image_url != ""}
 		if(!card.nil?)
@@ -13,5 +15,11 @@ get "/mtg-bot" do
 			text = name + " had no matches with images."
 		end
 	end
-	"{text:" + text + "}"
+
+	content_type :json
+	{ 
+		:response_type => "in_channel",
+		:text => text, 
+		:attachments => []
+	}.to_json
 end
